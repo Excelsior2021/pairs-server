2763d0ebfc682884cb2ac659d589a656ea831c71
Jonathan Kila <temidee11@yahoo.co.uk>
2023-01-26 22:40:02 +0000

    configure rooms/sessions

--- server.js
+++ server.js
@@ -18,39 +18,59 @@ io.on("connection", socket => {
   players.push(socket.id)
   console.log(players)
 
-  if (players.length <= 2) socket.emit("setPlayer", players.length)
-
-  if (players.length === 2) {
+  socket.on("join_session", sessionID => {
+    socket.join(sessionID)
     const initialGameState = game.startGame()
     const playerTurn = "player1"
-    io.emit("start", initialGameState, playerTurn)
-  }
+    socket.emit("setPlayer", 2)
+    io.sockets
+      .in(sessionID)
+      .emit("start", initialGameState, playerTurn, sessionID)
+    // console.log(socket.rooms)
+  })
 
-  socket.on("player_request", (player, card) => {
+  socket.on("create_session", sessionID => {
+    let sessionPlayers = []
+    socket.join(sessionID.toString())
+    sessionPlayers.push(socket.id)
+
+    console.log("sessionPlayers: ", sessionPlayers)
+    // console.log(sessionID)
+
+    if (sessionPlayers.length <= 2) socket.emit("setPlayer", 1)
+  })
+
+  socket.on("player_request", (player, card, sessionID) => {
+    console.log(sessionID)
     const playerRequest = { player, card }
-    socket.broadcast.emit("player_requested", playerRequest)
+    socket.to(sessionID).emit("player_requested", playerRequest)
   })
 
   socket.on(
     "player_match",
-    (playerRequest, playerMatch, gameState, playerOutput) => {
+    (playerRequest, playerMatch, gameState, playerOutput, sessionID) => {
       const newGameState = game.handlePlayerMatchPairs(
         playerRequest,
         playerMatch,
         gameState
       )
 
-      io.emit("player_match", newGameState, playerOutput, playerRequest.player)
+      io.in(sessionID).emit(
+        "player_match",
+        newGameState,
+        playerOutput,
+        playerRequest.player
+      )
     }
   )
 
-  socket.on("no_player_match", playerRequest =>
-    socket.broadcast.emit("player_to_deal", playerRequest)
+  socket.on("no_player_match", (playerRequest, sessionID) =>
+    socket.to(sessionID).emit("player_to_deal", playerRequest)
   )
 
   socket.on(
     "player_dealt",
-    (dealtCard, shuffledDeck, playerRequest, gameState) => {
+    (dealtCard, shuffledDeck, playerRequest, gameState, sessionID) => {
       const { gameState: newGameState, playerOutput } = game.handleDealtCard(
         dealtCard,
         shuffledDeck,
@@ -58,21 +78,23 @@ io.on("connection", socket => {
         gameState
       )
 
-      io.emit("player_dealt", newGameState, playerOutput, playerRequest.player)
+      io.sockets
+        .in(sessionID)
+        .emit("player_dealt", newGameState, playerOutput, playerRequest.player)
     }
   )
 
-  socket.on("player_response_message", playerOutput =>
-    socket.broadcast.emit("player_response_message", playerOutput)
+  socket.on("player_response_message", (playerOutput, sessionID) =>
+    socket.to(sessionID).emit("player_response_message", playerOutput)
   )
 
-  socket.on("player_turn_switch", () =>
-    socket.broadcast.emit("player_turn_switch")
+  socket.on("player_turn_switch", sessionID =>
+    socket.to(sessionID).emit("player_turn_switch")
   )
 
   socket.on("disconnect", () => {
     players = players.filter(player => player !== socket.id)
-    console.log(socket.connected, "disconnected", players)
+    console.log("disconnected", players)
   })
 })
 
